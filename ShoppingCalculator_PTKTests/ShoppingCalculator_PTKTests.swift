de6037f7793a612bf68253a15523d4f31e00d018
Pyi Theim Kyaw <k.pyitheim@wustl.edu>
2018-09-07 16:19:03 -0500

    Initial Commit

---
+++ ShoppingCalculator_PTKTests/ShoppingCalculator_PTKTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShoppingCalculator_PTKTests.swift
+//  ShoppingCalculator_PTKTests
+//
+//  Created by Pyi Theim Kyaw on 9/7/18.
+//  Copyright © 2018 Pyi Theim Kyaw. All rights reserved.
+//
+
+import XCTest
+@testable import ShoppingCalculator_PTK
+
+class ShoppingCalculator_PTKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
